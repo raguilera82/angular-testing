4afc62424342fbf19a23d9dad6185ed8909cdb9a
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2017-04-07 13:24:28 +0200

    All implemented in one component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnDestroy {
+
+  user: any;
+  error: string;
+  subs: Subscription;
+
+  constructor(private http: Http) { }
+
+  search(username: string): void {
+    this.user = null;
+    this.error = null;
+    this.subs = this.http.get(`https://api.github.com/users/${username}`)
+      .subscribe(
+      (response) => this.user = response.json(),
+      (error) => this.error = error
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
 }
